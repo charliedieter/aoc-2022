29fb59c383283100947a0d029cdf24229be33df5
Charlie Dieter <charlie@forethought.ai>
2022-12-03 21:43:35 -0800

    Add part 1

--- 3/index.js
+++ 3/index.js
@@ -2,6 +2,15 @@ const fs = require("fs");
 let lines = fs.readFileSync("3/input.txt").toString().split(/\n/);
 let pairs = lines.map((l) => l.split(",").map((p) => p.split("-").map(Number)));
 
+// let count = 0;
+// for (let [[x1, y1], [x2, y2]] of pairs) {
+//   let m1 = x1 <= x2 && y1 >= y2;
+//   let m2 = x1 >= x2 && y1 <= y2;
+//   if (m1 || m2) {
+//     count++;
+//   }
+// }
+
 let count = 0;
 for (let [[x1, y1], [x2, y2]] of pairs) {
   if (Math.max(x1, x2) <= Math.min(y1, y2)) {
